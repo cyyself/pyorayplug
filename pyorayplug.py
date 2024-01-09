e2ae5d92b495845b421e494eb343b75f130a69d7
Yangyu Chen <cyy@cyyself.name>
2024-01-09 16:53:58 +0800

    add support for proxies

--- pyorayplug.py
+++ pyorayplug.py
@@ -1,20 +1,21 @@
 import requests
 
 class orayplug:
-    def __init__(self, url, sn, key, time):
+    def __init__(self, url, sn, key, time, proxies=None):
         self.url = url
         self.sn = sn
         self.key = key
         self.time = time
+        self.proxies=proxies
     def query(self):
         try:
-            r = requests.get(f'{self.url}/plug?sn={self.sn}&_api=get_plug_status&key={self.key}&time={self.time}')
+            r = requests.get(f'{self.url}/plug?sn={self.sn}&_api=get_plug_status&key={self.key}&time={self.time}', proxies=self.proxies)
             return {x['index']: bool(x['status']) for x in r.json()['response']}
         except:
             return []
     def set(self, index, on: bool):
         try:
-            r = requests.get(f'{self.url}/plug?sn={self.sn}&_api=set_plug_status&key={self.key}&time={self.time}&index={index}&status={1 if on else 0}')
+            r = requests.get(f'{self.url}/plug?sn={self.sn}&_api=set_plug_status&key={self.key}&time={self.time}&index={index}&status={1 if on else 0}', proxies=self.proxies)
             return r.json()['result'] == 0
         except:
             return False
